dceb457b9f18047a9e936bd0552620ba519525e3
mertyasarerkoc <mert.erkoc@finkoto.com>
2023-03-09 01:04:30 +0300

    example code update

--- example/mongo_pool_example.dart
+++ example/mongo_pool_example.dart
@@ -1,19 +1,50 @@
-import 'package:mongo_pool/mongo_pool.dart';
+import 'package:mongo_pool/src/mongo_pool_service.dart';
 
 Future<void> main() async {
   /// Create a pool of 5 connections
-  final pool = MongoDbPool(5, 'mongodb://localhost:27017/my_database');
+  final poolService = MongoDbPoolService(
+    poolSize: 5,
+    mongoDbUri: 'mongodb://localhost:27017/my_database',
+  );
+
   /// Open the pool
-  await pool.open();
+  openDbPool(poolService);
+
   /// Get a connection from pool
-  final conn = await pool.acquire();
+  final conn = await poolService.acquire();
 
   // Database operations
   final collection = conn.collection('my_collection');
   final result = await collection.find().toList();
   // Connection release for other operations
-  pool.release(conn);
+  poolService.release(conn);
 
   // Pool close
-  await pool.close();
+  await poolService.close();
+}
+
+Future<void> openDbPool(MongoDbPoolService service) async {
+  try {
+    await service.open();
+  } on Exception catch (e) {
+    /// handle the exception here
+    print(e.toString());
+  }
+}
+
+class OtherClass {
+  OtherClass();
+
+  Future<void> openDbPool() async {
+    /// Get the instance of the pool
+    final poolService = MongoDbPoolService.getInstance();
+    final conn = await poolService.acquire();
+    // Database operations
+    final collection = conn.collection('my_collection');
+    final result = await collection.find().toList();
+    // Connection release for other operations
+    poolService.release(conn);
+    // Pool close
+    await poolService.close();
+  }
 }
