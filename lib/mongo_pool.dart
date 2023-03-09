840f5e4b20dd95f0fd4b0a47a26a3171deda92d7
mertyasarerkoc <mert.erkoc@finkoto.com>
2023-03-09 22:14:03 +0300

    test and export update

--- lib/mongo_pool.dart
+++ lib/mongo_pool.dart
@@ -1,4 +1,4 @@
 library mongo_pool;
 
-export 'src/mongo_pool_base.dart';
+export 'src/mongo_pool_service.dart';
 export 'package:mongo_dart/mongo_dart.dart';
--- test/mongo_pool_test.dart
+++ test/mongo_pool_test.dart
@@ -1,55 +1,12 @@
 import 'package:mongo_pool/mongo_pool.dart';
-import 'package:mongo_pool/src/mongo_pool_service.dart';
 import 'package:test/test.dart';
 
 void main() {
   /// Test the MongoDbPool class
   group('MongoDbPool tests', () {
-    late MongoDbPool pool;
 
     setUp(() {
       /// Create a pool of 2 connections
-      pool = MongoDbPool(2, 'mongodb://localhost:27017/station_center');
-    });
-
-    /// Test the MongoDbPoolService class
-    test('Acquire and release connections', () async {
-      final conn1 = await pool.acquire();
-      final conn2 = await pool.acquire();
-      expect(pool.available.length, equals(0));
-      expect(pool.inUse.length, equals(2));
-      pool.release(conn1);
-      expect(pool.available.length, equals(1));
-      expect(pool.inUse.length, equals(1));
-      pool.release(conn2);
-      expect(pool.available.length, equals(2));
-      expect(pool.inUse.length, equals(0));
-    });
-
-    /// Test the MongoDbPoolService class
-    test('No connection available', () async {
-      final conn1 = await pool.acquire();
-      final conn2 = await pool.acquire();
-      expect(pool.available.length, equals(0));
-      expect(pool.inUse.length, equals(2));
-      final conn3 = await pool.acquire();
-      expect(conn3.state, equals(State.open));
-      expect(pool.available.length, equals(0));
-      pool.release(conn3);
-      expect(pool.available.length, equals(1));
-    });
-
-    /// Test the MongoDbPoolService class
-    test('Close pool', () async {
-      final conn1 = await pool.acquire();
-      final conn2 = await pool.acquire();
-      expect(pool.available.length, equals(0));
-      expect(pool.inUse.length, equals(2));
-      await pool.close();
-      expect(pool.available.length, equals(0));
-      expect(pool.inUse.length, equals(0));
-      expect(conn1.state, equals(State.closed));
-      expect(conn2.state, equals(State.closed));
     });
 
     /// Test the MongoDbPoolService class
