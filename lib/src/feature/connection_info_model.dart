f75efc3d83a73dd8f1ab4e2cd5f0147f48955e3e
mertyasarerkoc <mertyasarerkocc@gmail.com>
2023-08-20 03:31:09 +0300

    connection leak handling init

--- lib/src/feature/connection_info_model.dart
+++ lib/src/feature/connection_info_model.dart
@@ -1,10 +1,23 @@
 import 'package:mongo_pool/mongo_pool.dart';
+import 'package:mongo_pool/src/feature/connection_leak/proxy_leak_task.dart';
 
 class ConnectionInfo {
-  ConnectionInfo(this.connection)
-      : createTime = DateTime.now(),
-        lastUseTime = DateTime.now();
+  ConnectionInfo(
+    this.connection, {
+    required this.lastUseTime,
+    required this.proxyLeakTask,
+  }) {
+    createTime = DateTime.now();
+  }
+
   Db connection;
-  DateTime createTime;
+  late final DateTime createTime;
   DateTime lastUseTime;
+  late final ProxyLeakTask proxyLeakTask;
+
+  /// Connection last used time
+  DateTime get lastUse => lastUseTime;
+
+  /// Last time the connection was used.
+  set lastUse(DateTime time) => lastUseTime = time;
 }
