be0c7d1da0da2c9d8879aa90ccf26726ff5ce2ff
mertyasarerkoc <mertyasarerkocc@gmail.com>
2023-08-25 00:04:31 +0300

    mongo pool already opened bug fix

---
+++ test/connection_already_opened_test.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:mongo_pool/mongo_pool.dart';
+import 'package:mongo_pool/src/exception/exception.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final mongoDbUri = Platform.environment['MONGODB_URI'] ??
+      'mongodb://localhost:27017/my_database';
+  setUp(
+    () async {
+      final pool = MongoDbPoolService(
+        MongoPoolConfiguration(
+          poolSize: 4,
+          uriString: mongoDbUri,
+          maxLifetimeMilliseconds: 10000,
+        ),
+      );
+      await pool.open();
+    },
+  );
+
+  group('Connection already opened test', () {
+    /// Test the MongoDbPoolService class
+    test('Open pool', () async {
+      final mongoDb = MongoDbPoolService.getInstance();
+     try {
+       await mongoDb.open();
+     } on PoolAlreadyOpenMongoPoolException catch (e) {
+       expect(e.message, equals('Pool is already open'));
+     }
+    });
+  });
+}
